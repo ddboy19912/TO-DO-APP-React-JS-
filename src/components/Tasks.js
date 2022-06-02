b13d35efe03f2a167ef61ed7c04dc95ce79caee0
Fortune <ddboy19912@gmail.com>
2022-06-02 13:11:16 +0100

    First commit

---
+++ src/components/Tasks.js
@@ -0,0 +1,33 @@
+import Task from "./Task"
+
+// const tasks = [
+//    { id: 1,
+//     text: 'Doctors Appointment',
+//     day: 'Feb 5th at 2:30pm',
+//     reminder: true},
+
+//       { id: 2,
+//     text: 'Meeting at School',
+//     day: 'Feb 6th at 1:30pm',
+//     reminder: true},
+
+//       { id: 3,
+//     text: 'Food Shooping',
+//     day: 'Feb 5th at 2:30pm',
+//     reminder: false,}
+
+// ] 
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+   
+
+  return (
+    <>
+{tasks.map((task) => (
+<Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+))}
+    </>
+  )
+}
+
+export default Tasks
